aaed54f0f8de02705add47ad8a9d4cd62708ceae
D20CM <scottbrown500@hotmail.com>
2022-02-10 12:08:11 +0000

    establish socket connection

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 import express from "express";
-// import { Server } from "socket.io";
-// const io = new Server(server);
-
+import http from "http";
 import path from "path";
 import { fileURLToPath } from "url";
+import { Server } from "socket.io";
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000);
 
 console.log("app is listening");
